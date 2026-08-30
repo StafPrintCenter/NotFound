8e10d68acbecb95339d162beefeb5397a6c450e9
steveasterafovo <steveasterafovo@gmail.com>
2026-08-31 00:10:27 +0100

    s

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -4,3 +4,4 @@ export { DiagnosticPanel } from "./DiagnosticPanel";
 export { PlatformCard } from "./PlatformCard";
 export { HeroSection } from "./HeroSection";
 export { PlatformHub } from "./PlatformHub";
+export { PlatformSkeleton } from "./PlatformSkeleton";
