b797ae1f7d353cfaa8b083f1adcabe79eb61d032
steveasterafovo <steveasterafovo@gmail.com>
2026-08-31 01:21:43 +0100

    s

--- src/components/pages/diagnostic/index.ts
+++ src/components/pages/diagnostic/index.ts
@@ -1,4 +1,3 @@
-export { PlatformSearch } from "./PlatformSearch";
 export { DiagnosticHeader } from "./DiagnosticHeader";
 export { DiagnosticRow, LocationRow, LocalTime, useDetectedRegion, useLocalTime } from "./DiagnosticInfo";
 export { DiagnosticTip } from "./DiagnosticTip";
