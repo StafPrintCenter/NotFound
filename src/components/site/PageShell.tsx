1d9312c9229263d86f9acd9bb38a0fe7a6944153
steveasterafovo <steveasterafovo@gmail.com>
2026-09-15 16:38:50 +0100

    s

---
+++ src/components/site/PageShell.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from "react";
+import { Header, Footer } from ".";
+
+export function BriefShell({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1 py-8">{children}</main>
+      <Footer />
+    </div>
+  );
+}
